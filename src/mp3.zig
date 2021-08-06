46ba1c8c8fe327dbc2c30ad41f3cf0fca62a5c33
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-08-06 10:35:56 +0200

    Add data conversion

--- src/mp3.zig
+++ src/mp3.zig
@@ -3,14 +3,14 @@ const Levels = struct {
     trebble: u8,
     medium: u8,
     bass: u8,
-}
+};
 
 const Selector = struct {
     menu: bool,
     one: bool,
     two: bool,
     three: bool,
-}
+};
 
 const Half = struct {
     jog: u8,
@@ -23,17 +23,71 @@ const Half = struct {
     track_next: bool,
     track_previous: bool,
     pitch: u8,
-    headphones: bool,
+    headset: bool,
     auto_beat: bool,
     load: bool,
     beat_lock: bool,
     menu: Selector,
-}
+};
 
-const MP3 = struct {
+pub const Controller = struct {
     left: Half,
     right: Half,
     crossfader: u8,
     mouse_x: u8,
     mouse_y: u8,
-}
+
+    pub fn update(this: *Controller, buffer: *[20]u8) void {
+        this.mouse_x = buffer[19];
+        this.mouse_y = buffer[18];
+
+        this.crossfader = buffer[11];
+
+        this.right.jog = buffer[17];
+        this.left.jog = buffer[16];
+
+        this.right.volume = buffer[13];
+        this.left.volume = buffer[12];
+
+        this.right.levels.bass = buffer[5];
+        this.right.levels.medium = buffer[6];
+        this.right.levels.trebble = buffer[7];
+
+        this.left.levels.bass = buffer[8];
+        this.left.levels.medium = buffer[9];
+        this.left.levels.trebble = buffer[10];
+
+        this.right.pitch = buffer[15];
+        this.right.pitch = buffer[14];
+
+        this.right.play = buffer[1] & 2 != 0;
+        this.right.cue = buffer[1] & 4 != 0;
+        this.right.track_next = buffer[1] & 32 != 0;
+        this.right.track_previous = buffer[1] & 16 != 0;
+        this.right.pitch_plus = buffer[3] & 64 != 0;
+        this.right.pitch_minus = buffer[3] & 128 != 0;
+        this.right.headset = buffer[4] & 1 != 0;
+        this.right.auto_beat = buffer[1] & 8 != 0;
+        this.right.load = buffer[4] & 8 != 0;
+        this.right.beat_lock = buffer[4] & 2 != 0;
+        this.right.menu.menu = buffer[1] & 1 != 0;
+        this.right.menu.one = buffer[2] & 128 != 0;
+        this.right.menu.two = buffer[3] & 1 != 0;
+        this.right.menu.three = buffer[3] & 2 != 0;
+
+        this.left.play = buffer[1] & 128 != 0;
+        this.left.cue = buffer[2] & 1 != 0;
+        this.left.track_next = buffer[2] & 8 != 0;
+        this.left.track_previous = buffer[2] & 4 != 0;
+        this.left.pitch_plus = buffer[3] & 4 != 0;
+        this.left.pitch_minus = buffer[3] & 8 != 0;
+        this.left.headset = buffer[3] & 16 != 0;
+        this.left.auto_beat = buffer[2] & 2 != 0;
+        this.left.load = buffer[4] & 4 != 0;
+        this.left.beat_lock = buffer[3] & 32 != 0;
+        this.left.menu.menu = buffer[1] & 64 != 0;
+        this.left.menu.one = buffer[2] & 64 != 0;
+        this.left.menu.two = buffer[2] & 32 != 0;
+        this.left.menu.three = buffer[2] & 16 != 0;
+    }
+};
--- src/usb.zig
+++ src/usb.zig
@@ -6,27 +6,18 @@ const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 const allocator = &arena.allocator;
-
-const Device = struct {
-    product_id: u16,
-    interface_number: u8,
-    ep_in: u8,
-    ep_out: u8,
-    ep_in2: u8,
-    interface_number2: u8,
-};
-
-const HERCULES_MP3 = Device { .product_id = 0xd001, .interface_number = 2, .ep_in = 0x83, .ep_out = 0x01, .ep_in2 = 0, .interface_number2 = 4 };
-
-const HERCULES_VENDOR_ID: u16 = 0x06f8;
+const mem = std.mem;
+const mp3 = @import("mp3.zig");
 
 var HID_DEVICE: c.hid_device = undefined;
 
 const MAX_STR: c_int = 255;
 
+var MP3_STATE: mp3.Controller = undefined;
+
 pub fn setup() !void {
     var str: [4 * MAX_STR:0]u8 align(4) = undefined;
-    var buffer: [65]u8 = undefined;
+    var buffer: [2][20]u8 = undefined;
     
     var res = c.hid_init();
     try stdout.print("Got res : {x}\n", .{@intCast(usize, res)});
@@ -47,19 +38,26 @@ pub fn setup() !void {
 
     res = c.hid_get_indexed_string(handle, 1, @ptrCast([*c]c_int, &str), MAX_STR);
     try stdout.print("Device indexed string 1 : {s}\n", .{str});
+
+    var buffer_index: usize = 0;
+
+    //res = c.hid_set_nonblocking(handle, 1);
     
     while (true) {
         // Read requested state
-        res = c.hid_read(handle, @ptrCast([*c] u8, &buffer), 65);
+        res = c.hid_read(handle, @ptrCast([*c] u8, &(buffer[buffer_index])), 20);
 
         // Print out the returned buffer.
-        if (res > 0) {
+        if (res > 0 and !mem.eql(u8, &buffer[buffer_index], &buffer[1 - buffer_index])) {
             try stdout.print("res : {}. Got bytes :", .{res});
             var i: usize = 0;
             while (i < res) : (i += 1) {
-                try stdout.print(" {b:0>8}", .{buffer[i]});
+                try stdout.print(" {d:0>8}", .{buffer[buffer_index][i]});
             }
             try stdout.print("\n", .{});
+            MP3_STATE.update(&buffer[buffer_index]);
         }
+
+        buffer_index = 1 - buffer_index;
     }
 }
