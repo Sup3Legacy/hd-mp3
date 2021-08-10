45f77980ea0c6e7d5d46a3d414c4ab064144be26
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-08-10 19:53:44 +0200

    Add Priority struct

--- src/priority_queue.zig
+++ src/priority_queue.zig
@@ -3,13 +3,41 @@ pub const PriorityQueueError = error {
     QueueFull,
 };
 
-pub fn PriorityQueue(comptime T: type, comptime Capacity: usize) type {
+pub fn Priority(comptime T: type, comptime P: type) type {
     return struct {
+        priority: P,
+        value: T,
+        compFunc: fn(*P, *P) bool,
+
+        pub fn new(v: P, p: P, f: fn(*P, *P) bool) @This() {
+            return @This() {
+                .priority = p,
+                .value = v,
+                .compFunc = f,
+            }
+        }
+    }
+}
+
+pub fn PriorityQueue(comptime T: type, comptime P: type, comptime Capacity: usize) type {
+    return struct {
+        priority_type: type,
         values: [Capacity]T,
         size: usize,
         // compFunc(a, b) <=> a < b (if min-queue)
         compFunc: fn(*T, *T) bool,
 
+        // Returns a fresh PriorityQueue
+        pub fn new(fun: fn(*T, *T) bool) @This() {
+            return @This() {
+                .priority_type = Priority(T, P),
+                .values = [Capacity]T,
+                .size = 0,
+                .compFunc = fun,
+            };
+        }
+
+        // Returns pointer to the top value
         pub fn peak_top(this: *@This()) PriorityQueueError!*T {
             if (this.size == 0) {
                 return PriorityQueueError.QueueEmpty;
@@ -18,16 +46,20 @@ pub fn PriorityQueue(comptime T: type, comptime Capacity: usize) type {
             }
         }
 
+        // Enqueue a value
         pub fn enqueue(this: *@This(), data: T) PriorityQueueError!void {
             if (this.size == Capacity) {
                 return PriorityQueueError.QueueFull;
             }
         }
 
-        pub fn dequeue(this: *This()) PriorityQueueError!T {
+        // Dequeues a value
+        pub fn dequeue(this: *@This()) PriorityQueueError!T {
             if (this.size == 0) {
                 return PriorityQueueError.QueueEmpty;
             }
         }
+
+        pub fn update_priority()
     };
 }
